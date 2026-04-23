8d8e3979edb043c63d3606cb7dcdc369600df987
Manuel Muzembela <mmuzembe@student.42luanda.com>
2026-04-23 12:07:27 +0100

    computer choice done at 80%

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+
 console.log("Hello World")
+
+
+
+function getComputerChoice()
+{
+	const choices = ["pedra", "papel", "tesoura"]
+	const compchoice = choices[Math.floor(Math.random()*choices.length)]
+	return compchoice
+}	
+console.log(getComputerChoice())
