28dc0eb32453fa85f5d0fb1bee6d36803eb34784
Manuel Muzembela <mmuzembe@student.42luanda.com>
2026-04-23 12:17:31 +0100

    the human choice done

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 console.log("Hello World")
-
+let humanScore = 0;
+let computerScore = 0;
 
 
 function getComputerChoice()
@@ -9,4 +10,20 @@ function getComputerChoice()
 	const compchoice = choices[Math.floor(Math.random()*choices.length)]
 	return compchoice
 }	
+
+function getHumanChoice() {
+    let hmchoice = prompt("Escolha Uma Opção (Pedra, Papel ou Tesoura):");
+    
+    hmchoice = hmchoice.toLowerCase();
+
+    if (hmchoice === "pedra" || hmchoice === "papel" || hmchoice === "tesoura") {
+        return hmchoice;
+    } else {
+        console.log("Opção inválida! Tente novamente.");
+        return getHumanChoice();
+    }
+}
+
+
 console.log(getComputerChoice())
+console.log(getHumanChoice())
